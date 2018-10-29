b00d0dc724ccc07dfa906b9d44b8ab508a9533db
admin <admin@Win7_1TB>
2018-10-29 09:20:57 +0100

    He modificado el segundo mensaje

--- Hola_MundoGit/src/hola_mundogit/Hola_MundoGit.java
+++ Hola_MundoGit/src/hola_mundogit/Hola_MundoGit.java
@@ -16,6 +16,7 @@ public class Hola_MundoGit {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Yago es tonto");
     }
     
 }
